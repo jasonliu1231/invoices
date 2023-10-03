bf9e225201ba59fa74ac52c1c678a4b83f6c7c44
劉Jason <jason@zhis.com>
2023-10-03 18:07:32 +0800

    修正一下，讓樣本顯示是哪個xml

--- public/javascripts/xmlsample.js
+++ public/javascripts/xmlsample.js
@@ -2,7 +2,7 @@ $(async function () {
     const url = new URL(location.href);
     const id = url.searchParams.get("id");
     const type = url.searchParams.get("type");
-
+    $("#type").html(type);
     const response = await fetch(`/creatxml/${type}/${id}`);
     if (response.ok) {
         const data = await response.text();
